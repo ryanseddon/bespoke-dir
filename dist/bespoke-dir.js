58839a990c19d02aa3edf40d3f5c4171ffe1a6af
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2014-08-07 07:57:02 +1000

    Update for Bespoke.js v1.0 API

--- dist/bespoke-dir.js
+++ dist/bespoke-dir.js
@@ -1,14 +1,15 @@
 /*!
  * bespoke-dir v0.0.0
- * https://github.com/ryanseddon/bespoke-dir
  *
- * Copyright 2013, Ryan Seddon
+ * Copyright 2014, Ryan Seddon
  * This content is released under the MIT license
+ * 
  */
 
-(function(bespoke) {
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var n;"undefined"!=typeof window?n=window:"undefined"!=typeof global?n=global:"undefined"!=typeof self&&(n=self);var o=n;o=o.bespoke||(o.bespoke={}),o=o.plugins||(o.plugins={}),o.dir=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
+module.exports = function() {
 
-  bespoke.plugins.dir = function(deck) {
+  return function(deck) {
     var parent = deck.parent,
         lastSlide = 0;
 
@@ -37,4 +38,8 @@
     });
   };
 
-}(bespoke));
+};
+
+},{}]},{},[1])
+(1)
+});
